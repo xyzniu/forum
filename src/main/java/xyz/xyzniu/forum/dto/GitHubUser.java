86208596d1c52b97cf75d2e4b8e35ac1e747d604
xinyue niu <xinyue.z.niu@gmail.com>
2020-07-06 15:54:59 +0800

    add lombok support

--- src/main/java/xyz/xyzniu/forum/dto/GitHubUser.java
+++ src/main/java/xyz/xyzniu/forum/dto/GitHubUser.java
@@ -1,32 +1,10 @@
 package xyz.xyzniu.forum.dto;
 
+import lombok.Data;
+
+@Data
 public class GitHubUser {
-    
     private String login;
     private String id;
     private String name;
-    
-    public String getLogin() {
-        return login;
-    }
-    
-    public void setLogin(String login) {
-        this.login = login;
-    }
-    
-    public String getId() {
-        return id;
-    }
-    
-    public void setId(String id) {
-        this.id = id;
-    }
-    
-    public String getName() {
-        return name;
-    }
-    
-    public void setName(String name) {
-        this.name = name;
-    }
 }
