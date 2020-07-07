a400219e6c23f2c63bb23f5cf1ac350e340ba219
xinyue niu <xinyue.z.niu@gmail.com>
2020-07-07 16:07:20 +0800

    add mybatis generator support

--- src/main/java/xyz/xyzniu/forum/ForumApplication.java
+++ src/main/java/xyz/xyzniu/forum/ForumApplication.java
@@ -1,9 +1,11 @@
 package xyz.xyzniu.forum;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan(basePackages = "xyz.xyzniu.forum.mapper")
 public class ForumApplication {
     
     public static void main(String[] args) {
